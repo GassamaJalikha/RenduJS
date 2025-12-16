9a27d59b5e85749d62cc5a372bb2cfb68bd7e895
GassamaJalikha <Jalikha.gassama@latoile.em-lyon.com>
2025-12-16 12:29:21 +0100

    ok

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-
+// ETAPE 1 : Faire le HTML - structure (header, main et footer, nav avec lien cliquable)
+// ETAPE 2 : Parcourir le tableau JSON (mettre le fetch)
 const API_URL = `https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/menuiserie.json`
 fetch(API_URL)
     .then(response => response.json())
     .then(data => {
-
-
-
-
-
-
-
-
-        // ETAPE 1 : Faire le HTML - structure (header, main et footer, nav avec lien cliquable)
-        // ETAPE 2 : Parcourir le tableau JSON (mettre le fetch)
-        // etape 3 : nom de l'entreprise (h1), slogan (H2) et les boutons
+        // ETAPE 3 : nom de l'entreprise (h1), slogan (H2) et les boutons
         // ETAPE 4 : Pour chaque élement du tableau "réalisation"créer une DIV 
         // - Créer un titre (h3) pour afficher le nom
+        let title = document.createElement("h3")
         // - Créer un paragraphe (p) pour la description
         // - Créer un élément image (img) pour afficher la photo
         // ETAPE 5 : Envoyer dans le HTML les élements crées (appenchild)
